ca2310310ca9b19d93db3b1775a5e29c8e720c60
Jorge Goris <J_goris@live.com>
2020-02-19 19:26:52 -0500

    Changed sizing of Hamburger icon and padding on nav links

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ $(function() {
     $(".toggle").on("click", function() {
         if ($(".item").hasClass("active")) {
             $(".item").removeClass("active");
-            $(this).find("a").html("<i class='fas fa-bars'></i>");
+            $(this).find("a").html("<i class='fas fa-bars fa-lg'></i>");
         } else {
             $(".item").addClass("active");
-            $(this).find("a").html("<i class='fas fa-times'></i>");
+            $(this).find("a").html("<i class='fas fa-times fa-lg'></i>");
         }
     });
 })
