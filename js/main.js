ea99e26bc89605e50581d71ac97fe80eefdfdc21
Jorge Goris <J_goris@live.com>
2020-02-19 19:33:35 -0500

    Changed sizes yet again.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ $(function() {
     $(".toggle").on("click", function() {
         if ($(".item").hasClass("active")) {
             $(".item").removeClass("active");
-            $(this).find("a").html("<i class='fas fa-bars fa-lg'></i>");
+            $(this).find("a").html("<i class='fas fa-bars fa-2x'></i>");
         } else {
             $(".item").addClass("active");
-            $(this).find("a").html("<i class='fas fa-times fa-lg'></i>");
+            $(this).find("a").html("<i class='fas fa-times fa-2x'></i>");
         }
     });
 })
